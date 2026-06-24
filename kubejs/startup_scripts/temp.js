7d5d22bc2e1a946c73a582b8ab75198e7704ff19
UltraPuPower1 <ultrapupower1@gmail.com>
2026-06-24 10:40:18 +0200

    multi layer block works (thanks to @sgtbowlingball on discord)

--- kubejs/startup_scripts/temp.js
+++ kubejs/startup_scripts/temp.js
@@ -10,11 +10,19 @@ StartupEvents.registry('item', event => {
 StartupEvents.registry('block', event => {
 
     let newComponent = event.create('multi_layer_block')
-        .displayName(toDisplayName('multi_layer_block'))
-        .parentModel("kubejs:block/two_layer")
-        // .texture('layer0', `kubejs:item/materiallib/default/block`)
-        .color(0, 0x337E64)
-        // .texture(`layer1`, `kubejs:item/materiallib/default/ingot`)
-        .color(1, 0x333E7E);
+        .modelGenerator(m => {
+            m.parent("kubejs:block/layered_block");
+            m.texture('all', `kubejs:item/materiallib/default/block`);
+            m.texture('overlay', 'kubejs:item/materiallib/default/ingot');
+            m.element(e => {
+                e.allFaces(f => f.tex('#all').tintindex(0).cull());
+            });
+            m.element(e => {
+                e.allFaces(f => f.tex('#overlay').tintindex(1).cull());
+            });
+        })
+        .color(0, '#337E64')
+        .color(1, '#333E7E')
+        .item((i) => i.color(0, '#337E64').color(1, '#333E7E'))
     
 });
